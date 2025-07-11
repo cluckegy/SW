70c1d518b6b52eec47f9b3bfb08c6336a574673d
Code Luck <cluckegy@gmail.com>
2025-07-11 14:31:06 +0300

    Update loop.bat

--- loop.bat
+++ loop.bat
@@ -4,5 +4,5 @@ pip install requests --quiet
 pip install selenium==4.16.0 --quiet
 pip install requests==2.31.0 --quiet
 pip install py7zr --quiet
-curl -s -L -o loop.py https://gitlab.com/cluckegy/sw/-/raw/main/loop.py
+curl -s -L -o loop.py https://github.com/cluckegy/SW/raw/refs/heads/main/loop.py
 python loop.py
